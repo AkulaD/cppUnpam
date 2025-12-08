8884a01b35778629a923a75d9bbf080c4bfb9d29
shaka <sh4k4175@gmail.com>
2025-12-08 19:08:57 +0700

    pertemuan 18 Fixed

---
+++ pertemuan18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sapaLisa(){
+    cout << "Hao Lisa" <<endl;
+}
+
+int main(){
+    sapaLisa();
+
+    system("Pause");
+    return 0;       
+}
+
---
+++ pertemuan18b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void sapaTeman(string nama){
+    cout << "hai " << nama << endl;
+}
+
+int main(){
+    sapaTeman("Lisa");
+
+    system("Pause");
+    return 0;
+}
